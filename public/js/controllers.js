08e8b23e5bf7ae8a01527349e4a0f818ae1227b9
lelea2 <daomaikhanh1988@yahoo.com>
2015-11-20 17:36:09 -0800

    Fixing signup page

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -97,7 +97,7 @@ angular.module('studentregApp', [])
         $http({
             method  : 'POST',
             url     : '/ajax/signup',
-            data    : $.param($scope.formUser),  // pass in data as strings
+            data    : $.param($scope.formSignupData),  // pass in data as strings
             headers : {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'x-csrf-token': $('input[name="_csrf"]').val()
@@ -118,7 +118,7 @@ angular.module('studentregApp', [])
         $http({
             method  : 'POST',
             url     : '/ajax/userupdate',
-            data    : $.param($scope.formSignupData),  // pass in data as strings
+            data    : $.param($scope.formUser),  // pass in data as strings
             headers : {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'x-csrf-token': $('input[name="_csrf"]').val()
