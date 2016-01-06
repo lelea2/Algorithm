a60c04fe199427ef773a139f67e083778e1d1497
lelea2 <daomaikhanh1988@yahoo.com>
2016-01-06 09:28:25 -0800

    Finish apply-bind-call

---
+++ apply-call-bind/bind.js
@@ -0,0 +1,23 @@
+/**
+ * Experiment with bind, object for deduct method as example below
+ */
+
+var monica = {
+    name: 'Monica Geller',
+    total: 400,
+    deductMontlyFee: function(fee){
+        this.total = this.total - fee;
+        return this.name + ' remaining balance is '+ this.total;
+    }
+};
+
+//Define another user object
+var rachel = {name: 'Rachel Green', total: 1500};
+
+//caculate deduct monthly fee for Rachel as below
+//Pattern: obj.funcname.bind(newObj, args);
+var rachelFeeDeductor = monica.deductMonthlyFee.bind(rachel, 200); //200 is deduct "fee" per month
+
+//Test case:
+rachelFeeDeductor(); //"Rachel Green remaining balance is 1300"
+rachelFeeDeductor(); //"Rachel Green remaining balance is 1100"
--- apply-call-bind/call.js
+++ apply-call-bind/call.js
@@ -9,8 +9,8 @@ function isTwoPassed() {
 }
 
 //Test case
-//isTwoPassed(1,4) //false
-//isTowPassed(5,3,1,2) //true
+isTwoPassed(1,4) //false
+isTowPassed(5,3,1,2) //true
 
 
 /**
@@ -20,11 +20,29 @@ function isTwoPassed() {
 function decorateLog(string) {
     var originalFunc = console.log;
     console.log = function(){
-        originalFunc.apply(console, [string].concat(Array.prototype.slice.call(arguments)));
+        originalFunc.apply(console, [string].concat([].slice.call(arguments))); //passing array argument, concat array here
     }
 }
 
 decorateLog('Info: ');
 //Test case:
-//console.log('custom console is here'); //=> Test: custom console is here
-//console.log('foo', 'bar'); //=> Test: foo bar
+console.log('custom console is here'); //=> Test: custom console is here
+console.log('foo', 'bar'); //=> Test: foo bar
+
+/**
+ * How could you set a prefix before everything you log? for example, if you log('my message') it will log: "(app) my
+ * message"
+ */
+
+//Idea: log() function could takes multiple of argument ==> use array.slice to change argument to array of argument
+//Shift originial array to make sure the fisrst index will always be "(app)"
+//input in console log, console.log with take list of array argument
+function log() {
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift('(app)');
+    console.log.apply(console, args); //passing array argument, therefore use apply
+}
+
+//Test case
+log('my message'); //(app) my message
+log('my message', 'your message'); //(app) my message your message
