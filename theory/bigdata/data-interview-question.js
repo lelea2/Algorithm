b244f4bf5145deb67639612187b3f73d2cdc44ff
lelea2 <daomaikhanh1988@yahoo.com>
2016-01-10 15:00:55 -0800

    Initial check in

--- theory/bigdata/data-interview-question.js
+++
@@ -1,9 +0,0 @@
-### 1. Famous 2 eggs dropped on the floor
-
-http://datagenetics.com/blog/july22012/index.html
-
-* One egg: start at first floor, floor by floor
-* Many egg: can walk binary-tree, devide posibility in half
-* 2 egg: optimal, create segments
-
-### 2.
