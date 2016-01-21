d350dfe0706f33e87b7c05b84d61e3b4274a27f0
lelea2 <daomaikhanh1988@yahoo.com>
2016-01-21 04:19:31 -0800

    Initial check in

--- java/coding/removeNthNode.java
+++ java/coding/removeNthNode.java
@@ -3,6 +3,8 @@
  */
 
 //Walking the list with 2 pointer
+//Walk first pointer to nth position first
+//Then walk 2 pointers in parallel
 public ListNode removeNthFromEnd(ListNode head, int n) {
     ListNode p = new ListNode(-1);
     p.next = head;
@@ -10,11 +12,11 @@ public ListNode removeNthFromEnd(ListNode head, int n) {
     ListNode q = head;
     head = p;
 
-    for(int i=0; i<n ;i++) {
+    for(int i=0; i<n; i++) {
         q = q.next;
     }
 
-    while(q != null){
+    while(q != null) {
         q = q.next;
         p = p.next;
     }
