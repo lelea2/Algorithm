30f9893a0d35ed1dfea16db98ac7f1512f12e63a
lelea2 <daomaikhanh1988@yahoo.com>
2016-01-26 14:59:03 -0800

    Initial check in

--- util/func/flattenArray.js
+++ util/func/flattenArray.js
@@ -16,3 +16,33 @@ function flatten (arr, resultArr) {
     return result;
 }
 
+
+//Flatten the array, linear time
+// This is done in a linear time O(n) without recursion
+// memory complexity is O(1) or O(n) if mutable param is set to false
+function flatten(array, mutable) {
+    var toString = Object.prototype.toString;
+    var arrayTypeStr = '[object Array]';
+
+    var result = [];
+    var nodes = (mutable && array) || array.slice();
+    var node;
+
+    if (!array.length) {
+        return result;
+    }
+
+    node = nodes.pop();
+
+    do {
+        if (toString.call(node) === arrayTypeStr) {
+            nodes.push.apply(nodes, node);
+        } else {
+            result.push(node);
+        }
+    } while (nodes.length && (node = nodes.pop()) !== undefined);
+
+    result.reverse(); // we reverse result to restore the original order
+    return result;
+}
+
