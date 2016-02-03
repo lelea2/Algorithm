c87b865bda9eac14e7820eaee9edc77119f03f78
lelea2 <daomaikhanh1988@yahoo.com>
2016-02-03 09:46:02 -0800

    Initial check in

--- util/func/sumFinder.js
+++ util/func/sumFinder.js
@@ -8,7 +8,7 @@ function sumFinder(arr, sum) {
     var len = arr.length;
     for(var i =0; i<len-1; i++) {
         for(var j = i+1;j<len; j++) {
-            if (arr[i] + arr[j] == sum) {
+            if (arr[i] + arr[j] === sum) {
                 return true;
             }
         }
