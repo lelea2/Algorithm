64478f91fca9bcd347e10dce482630c2f4d061a2
lelea2 <daomaikhanh1988@yahoo.com>
2016-02-25 15:44:36 -0800

    Initial check in

--- util/func/deepEqual.js
+++ util/func/deepEqual.js
@@ -2,4 +2,37 @@
  * Checking deepEqaul object in JS
  */
 
+function deepEqual (x, y) {
+    if ((typeof x == "object" && x != null) && (typeof y == "object" && y != null)) {
+        if (Object.keys(x).length != Object.keys(y).length) {
+            return false;
+        }
+        for (var prop in x) {
+            if (y.hasOwnProperty(prop)) {
+                if (!deepEqual(x[prop], y[prop])) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    } else if (x !== y) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+var obj = {here: {is: "an", other: "3"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an", other: "2"}, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an", other: "3"}, object: 2}));
+// → true
 
