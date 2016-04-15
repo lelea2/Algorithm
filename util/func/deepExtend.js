70943f86f7c96faf59f875a795e56878c4af2a8c
Dao <kdao@kdao01sjl.corp.homestore.net>
2016-04-15 16:34:52 -0700

    Initial check in for more functions in JS

---
+++ util/func/deepExtend.js
@@ -0,0 +1,44 @@
+/**
+ * Function handle for deep merge object
+ */
+
+//NOTE: isArray() support IE >= 9 version only
+//Solution will be in recursive mode
+
+function deepmerge(target, src) {
+  var array = Array.isArray(src); //checking if sort is array
+  var dst = array ? [] : {}; //assign destination variable
+  if (array) {
+    target = target || [];
+    dst = dst.concat(target);
+    src.forEach(function(e, i) {
+      if (typeof dst[i] === 'undefined') {
+        dst[i] = e;
+      } else if (typeof e === 'object') {
+        dst[i] = deepmerge(target[i], e);
+      } else {
+        if (target.indexOf(e) === -1) {
+          dst.push(e);
+        }
+      }
+    });
+  } else {
+    if (target && typeof target === 'object') {
+      Object.keys(target).forEach(function (key) {
+        dst[key] = target[key];
+      });
+    }
+    Object.keys(src).forEach(function (key) {
+      if (typeof src[key] !== 'object' || !src[key]) {
+        dst[key] = src[key];
+      } else {
+        if (!target[key]) {
+          dst[key] = src[key];
+        } else {
+          dst[key] = deepmerge(target[key], src[key]);
+        }
+      }
+    });
+  }
+  return dst;
+}
