12629a7ae347a934e202243b4a0fce6e26f7b02a
Dao <kdao@kdao01sjl.attlocal.net>
2016-06-17 20:31:37 -0700

    More clean up

--- util/func/sumFinder.js
+++ util/func/sumFinder.js
@@ -6,8 +6,8 @@
 //Expected running complexity it O(n^2)
 function sumFinder(arr, sum) {
     var len = arr.length;
-    for(var i =0; i<len-1; i++) {
-        for(var j = i+1;j<len; j++) {
+    for(var i = 0; i < len-1; i++) {
+        for(var j = i+1; j < len; j++) {
             if (arr[i] + arr[j] === sum) {
                 return true;
             }
