1d27f8055d2c946efab7dba6e0b7f3ca3fd14884
Dao <kdao@kdao01sjl.corp.homestore.net>
2016-07-05 09:29:10 -0700

    Initial check in for JS calculating days

--- theory/web/formatDate.js
+++ theory/web/formatDate.js
@@ -20,3 +20,31 @@ function formatDate(userDate) {
     returnDate = y + m + d;
     return returnDate;
 }
+
+
+/**
+ * Get date in month (caching version)
+ */
+
+var daysInMonth = (function() {
+    var cache = {};
+    return function(month, year) {
+        var entry = year + '-' + month;
+
+        if (cache[entry]) return cache[entry];
+
+        return cache[entry] = new Date(year, month, 0).getDate();
+    }
+})();
+
+
+//Checking is leap year to get number of days in years
+function daysInYear(year) {
+    if(year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+        // Leap year
+        return 366;
+    } else {
+        // Not a leap year
+        return 365;
+    }
+}
