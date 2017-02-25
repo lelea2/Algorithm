9dce2c4b59be470d553f770229acb62f7c927da4
khanh.dao <khanh.dao@move.com>
2017-02-24 19:07:53 -0800

    merge heads

--- util/js-data-structures-master/search/seq-search.js
+++ util/js-data-structures-master/search/seq-search.js
@@ -1,8 +1,8 @@
 function seqSearch(arr, data) {
-   for (var i = 0; i < arr.length; ++i) {
-      if (arr[i] == data) {
-         return true;
-      }
-   }
-   return false;
+  for (var i = 0; i < arr.length; ++i) {
+    if (arr[i] == data) {
+      return true;
+    }
+  }
+  return false;
 }
