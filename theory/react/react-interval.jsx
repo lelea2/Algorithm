caede7dbfd92d66899691d3e7c0181c0bf9bc12d
khanh.dao <khanh.dao@move.com>
2017-02-27 18:14:10 -0800

    React-interval

---
+++ theory/react/react-interval.jsx
@@ -0,0 +1,86 @@
+
+import React from 'react';
+
+export const ReactInterval = React.createClass({
+  propTypes: {
+    callback: React.PropTypes.func.isRequired,
+    enabled: React.PropTypes.bool,
+    timeout: React.PropTypes.number
+  },
+
+  getDefaultProps() {
+    return {
+      enabled: false,
+      timeout: 1000
+    };
+  },
+
+  componentDidMount() {
+    if (this.props.enabled) {
+      this.start();
+    }
+  },
+
+  shouldComponentUpdate({timeout, callback, enabled}) {
+    return (
+      this.props.timeout !== timeout ||
+      this.props.callback !== callback ||
+      this.props.enabled !== enabled
+    );
+  },
+
+  componentDidUpdate({enabled}) {
+    if (this.props.enabled !== enabled) {
+      if (this.props.enabled) {
+        this.start();
+      } else {
+        this.stop();
+      }
+    }
+  },
+
+
+  componentWillUnmount() {
+    this.stop();
+  },
+
+  callback() {
+    if (this.timer) {
+      this.props.callback();
+      this.start();
+    }
+  },
+
+  start() {
+    this.stop();
+    this.timer = setTimeout(this.callback, this.props.timeout);
+  },
+
+
+  stop() {
+    clearTimeout(this.timer);
+    this.timer = null;
+  },
+
+  render() {
+    return false;
+  }
+});
+
+const App = React.createClass({
+  getInitialState() {
+    return {count: 0};
+  },
+
+  render() {
+    const {count} = this.state;
+
+    return (
+      <div>
+        {count}
+        <ReactInterval timeout={1000} enabled={true}
+          callback={() => this.setState({count: this.state.count + 1})} />
+      </div>
+    );
+  }
+});
