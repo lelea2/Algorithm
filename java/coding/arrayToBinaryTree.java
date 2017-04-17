c0dfa0fd7b091aebc31fa7a5d54b0a772b7bad89
khanh.dao <khanh.dao@move.com>
2017-04-17 09:42:23 -0700

    Merge heads

---
+++ java/coding/arrayToBinaryTree.java
@@ -0,0 +1,68 @@
+// Java program to print BST in given range
+
+// A binary tree node
+
+//Runtime: O(n)
+// T(n) = 2T(n/2) + C
+// T(n) -->  Time taken for an array of size n
+//  C   -->  Constant (Finding middle of array and linking root to left
+//                     and right subtrees take constant time)
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+
+    static Node root;
+
+    /* A function that constructs Balanced Binary Search Tree
+     from a sorted array */
+    Node sortedArrayToBST(int arr[], int start, int end) {
+
+        /* Base Case */
+        if (start > end) {
+            return null;
+        }
+
+        /* Get the middle element and make it root */
+        int mid = (start + end) / 2;
+        Node node = new Node(arr[mid]);
+
+        /* Recursively construct the left subtree and make it
+         left child of root */
+        node.left = sortedArrayToBST(arr, start, mid - 1);
+
+        /* Recursively construct the right subtree and make it
+         right child of root */
+        node.right = sortedArrayToBST(arr, mid + 1, end);
+
+        return node;
+    }
+
+    /* A utility function to print preorder traversal of BST */
+    void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.data + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        int arr[] = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int n = arr.length;
+        root = tree.sortedArrayToBST(arr, 0, n - 1);
+        System.out.println("Preorder traversal of constructed BST");
+        tree.preOrder(root);
+    }
+}
+
--- theory/web/javascript-question.md
+++ theory/web/javascript-question.md
@@ -94,6 +94,7 @@
 - 48. Detect when image fails to load in Javascript
 - 49. Event Bus vs. Mediator pattern in JS
 - 50. Reduce Procedure
+- 51. Weakmap
 
 <!-- /MarkdownTOC -->
 
@@ -1871,4 +1872,63 @@ function add(a, b) {
 // add(6, 4)  => 10
 // add(10, 5) => 15
 
+```
+
+#### 51. Weakmap
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakMap
+
+```javascript
+var wm1 = new WeakMap(),
+    wm2 = new WeakMap(),
+    wm3 = new WeakMap();
+var o1 = {},
+    o2 = function() {},
+    o3 = window;
+
+wm1.set(o1, 37);
+wm1.set(o2, 'azerty');
+wm2.set(o1, o2); // a value can be anything, including an object or a function
+wm2.set(o3, undefined);
+wm2.set(wm1, wm2); // keys and values can be any objects. Even WeakMaps!
+
+wm1.get(o2); // "azerty"
+wm2.get(o2); // undefined, because there is no key for o2 on wm2
+wm2.get(o3); // undefined, because that is the set value
+
+wm1.has(o2); // true
+wm2.has(o2); // false
+wm2.has(o3); // true (even if the value itself is 'undefined')
+
+wm3.set(o1, 37);
+wm3.get(o1); // 37
+
+wm1.has(o1); // true
+wm1.delete(o1);
+wm1.has(o1); // false
+
+```
+
+```javascript
+class ClearableWeakMap {
+  constructor(init) {
+    this._wm = new WeakMap(init)
+  }
+  clear() {
+    this._wm = new WeakMap()
+  }
+  delete(k) {
+    return this._wm.delete(k)
+  }
+  get(k) {
+    return this._wm.get(k)
+  }
+  has(k) {
+    return this._wm.has(k)
+  }
+  set(k, v) {
+    this._wm.set(k, v)
+    return this
+  }
+}
 ```
