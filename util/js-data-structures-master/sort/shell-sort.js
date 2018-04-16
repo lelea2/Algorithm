2672b3dcde6a785520b8ee1c9bdfc71e1c4ff987
kdao <khanh.dao@move.com>
2018-04-16 13:38:38 -0700

    Merge heads

--- util/js-data-structures-master/sort/shell-sort.js
+++ util/js-data-structures-master/sort/shell-sort.js
@@ -2,33 +2,30 @@ load("CArray.js");
 CArray.prototype.shellSort = shellsort;
 CArray.prototype.gaps = [5,3,1];
 function shellsort() {
-   for (var g = 0; g < this.gaps.length; ++g) {
-      for (var i = this.gaps[g]; i < this.dataStore.length; ++i) {
-         var temp = this.dataStore[i];
-         for (var j = i; j >= this.gaps[g] &&
-                         this.dataStore[j-this.gaps[g]] > temp;
-              j -= this.gaps[g]) {
-            this.dataStore[j] = this.dataStore[j - this.gaps[g]];
-         }
-         this.dataStore[j] = temp;
+  for (var g = 0; g < this.gaps.length; ++g) {
+    for (var i = this.gaps[g]; i < this.dataStore.length; ++i) {
+      var temp = this.dataStore[i];
+      for (var j = i; j >= this.gaps[g] && this.dataStore[j-this.gaps[g]] > temp; j -= this.gaps[g]) {
+        this.dataStore[j] = this.dataStore[j - this.gaps[g]];
       }
-   }
+      this.dataStore[j] = temp;
+    }
+  }
 }
 
 CArray.prototype.shellsort1 = shellsort1;
 function shellsort1() {
-   var N = this.dataStore.length;
-   var h = 1;
-   while (h < N/3) {
-      h = 3 * h + 1;
-   }
-   while (h >= 1) {
-      for (var i = h; i < N; i++) {
-         for (var j = i; j >= h && this.dataStore[j] < this.dataStore[j-h];
-              j -= h) {
-            this.swap(this.dataStore, j, j-h);
-         }
+  var N = this.dataStore.length;
+  var h = 1;
+  while (h < N/3) {
+    h = 3 * h + 1;
+  }
+  while (h >= 1) {
+    for (var i = h; i < N; i++) {
+      for (var j = i; j >= h && this.dataStore[j] < this.dataStore[j-h];j -= h) {
+        this.swap(this.dataStore, j, j-h);
       }
-      h = (h-1)/3;
-   }
+    }
+    h = (h-1)/3;
+  }
 }
--- util/security/README.md
+++ util/security/README.md
@@ -10,3 +10,24 @@
 
 
 ##### 4. What is Phishing?
+
+
+##### 5. NodeJs key compare
+```
+function checkApiKey (apiKeyFromDb, apiKeyReceived) {
+  if (apiKeyFromDb === apiKeyReceived) {
+    return true
+  }
+  return false
+}
+```
+
+* When you compare security credentials it is crucial that you don't leak any information, so you have to make sure that you compare them in fixed time. If you fail to do so, your application will be vulnerable to timing attacks.
+
+* Fixing using `cryptiles` npm
+```javascript
+function checkApiKey (apiKeyFromDb, apiKeyReceived) {
+  return cryptiles.fixedTimeComparison(apiKeyFromDb, apiKeyReceived)
+}
+
+```
